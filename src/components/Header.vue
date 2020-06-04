56304da5705679c6f109531596044a947cb12f0e
zjc2233737325 <43945226+zjc2233737325@users.noreply.github.com>
2020-06-04 22:40:55 +0800

    header换肤

---
+++ src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="header" @click="changeTheme">
+    <div class="header-left"></div>
+    <p class="header-title">zjc音乐</p>
+    <div class="header-right"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      themes: ['theme', 'theme1', 'theme2'],
+      index: 0
+    }
+  },
+  methods: {
+    changeTheme () {
+      this.index++
+      if (this.index >= this.themes.length) {
+        this.index = 0
+      }
+      document.documentElement.setAttribute('data-theme', this.themes[this.index])
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import "../assets/css/variable";
+  @import "../assets/css/mixin";
+  .header{
+    width: 100%;
+    height: 100px;
+    /*background-color: red;*/
+    @include bg_color();
+    display: inline-flex;
+    justify-content: space-between;
+    .header-left, .header-right{
+      width: 84px;
+      height: 84px;
+      /*background-color: orange;*/
+      margin-top: 8px;
+    }
+    .header-left{
+      @include bg_img('../assets/images/logo')
+    }
+    .header-right{
+      @include bg_img('../assets/images/account')
+    }
+    .header-title{
+      text-align: center;
+      line-height: 100px;
+      color: #fff;
+      font-weight: bold;
+      @include font_size($font_medium)
+    }
+  }
+
+</style>
