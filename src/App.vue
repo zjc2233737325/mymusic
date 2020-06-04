1cd04f1891deb00e86834e044a283f6a9334cac6
zjc2233737325 <43945226+zjc2233737325@users.noreply.github.com>
2020-06-04 19:37:17 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <div class="test"></div>
+  </div>
+</template>
+
+<style lang="scss">
+.test{
+  width: 100px;
+  height: 100px;
+  background-color: red;
+}
+</style>
