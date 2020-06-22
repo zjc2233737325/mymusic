5c02c01c58203414a205509b79474a6614210a67
zjc2233737325 <43945226+zjc2233737325@users.noreply.github.com>
2020-06-22 10:01:30 +0800

    歌词同步，公众累类的抽取等

---
+++ src/views/Account.vue
@@ -0,0 +1,47 @@
+<template>
+  <transition appear>
+    <div class="account">
+
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'Account'
+}
+</script>
+
+<style scoped lang="scss">
+.account{
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background-color: red;
+}
+
+.v-enter{
+  transform: translateX(100%);
+  /*opacity: 0;*/
+}
+.v-enter-to{
+  transform: translateX(0%);
+  /*opacity: 1;*/
+}
+.v-enter-active{
+  transition: all 0.5s;
+}
+.v-leave{
+  /*opacity: 1;*/
+  transform: translateX(0%);
+}
+.v-leave-to{
+  /*opacity: 0;*/
+  transform: translateX(100%);
+}
+.v-leave-active{
+  transition: all 0.5s;
+}
+</style>
--- src/views/Player.vue
+++ src/views/Player.vue
@@ -1,9 +1,9 @@
 <template>
 <div class="player">
-  <NormalPlayer></NormalPlayer>
+  <NormalPlayer :totalTime="totalTime" :currentTime="currentTime"></NormalPlayer>
   <MiniPlayer></MiniPlayer>
   <ListPlayer ref="listPlayer"></ListPlayer>
-  <audio :src="currentSong.url" ref="audio"></audio>
+  <audio :src="currentSong.url" ref="audio" @timeupdate="timeupdate" @ended="end"></audio>
 </div>
 </template>
 
@@ -11,7 +11,9 @@
 import NormalPlayer from '../components/Player/NormalPlayer'
 import MiniPlayer from '../components/Player/MiniPlayer'
 import ListPlayer from '../components/Player/ListPlayer'
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
+import mode from '../store/modeType'
+import { getRandomIntInclusive, setLocalStorage, getLocalStorage } from '../tools/tools'
 export default {
   name: 'Player',
   components: {
@@ -23,12 +25,18 @@ export default {
     ...mapGetters([
       'currentSong',
       'isPlaying',
-      'currentIndex'
+      'currentIndex',
+      'curTime',
+      'modeType',
+      'songs',
+      'favoriteList',
+      'historyList'
     ])
   },
   watch: {
     isPlaying (newValue, oldValue) {
       if (newValue) {
+        this.setHistorySong(this.currentSong)
         this.$refs.audio.play()
       } else {
         this.$refs.audio.pause()
@@ -36,12 +44,71 @@ export default {
     },
     currentIndex () {
       this.$refs.audio.oncanplay = () => {
+        this.totalTime = this.$refs.audio.duration
         if (this.isPlaying) {
+          this.setHistorySong(this.currentSong)
           this.$refs.audio.play()
         } else {
-          // ni
           this.$refs.audio.pause()
         }
+      }
+    },
+    curTime (newValue, oldValue) {
+      this.$refs.audio.currentTime = newValue
+    },
+    favoriteList (newValue, oldValue) {
+      // window.localStorage.setItem('favoriteList', JSON.stringify(newValue))
+      setLocalStorage('favoriteList', newValue)
+    },
+    historyList (newValue, oldValue) {
+      // window.localStorage.setItem('historyList', JSON.stringify(newValue))
+      getLocalStorage('historyList', newValue)
+    }
+  },
+  created () {
+    // const favoriteList = JSON.parse(window.localStorage.getItem('favoriteList'))
+    const favoriteList = getLocalStorage('favoriteList')
+    if (favoriteList === null) {
+      return
+    }
+    this.setFavoriteList(favoriteList)
+    // const historyList = JSON.parse(window.localStorage.getItem('historyList'))
+    const historyList = getLocalStorage('historyList')
+    if (historyList === null) {
+      return
+    }
+    this.setHistoryList(historyList)
+  },
+  mounted () {
+    this.$refs.audio.oncanplay = () => {
+      this.totalTime = this.$refs.audio.duration
+    }
+  },
+  data () {
+    return {
+      totalTime: 0,
+      currentTime: 0
+    }
+  },
+  methods: {
+    ...mapActions([
+      'setCurrentIndex',
+      'setFavoriteList',
+      'setHistorySong',
+      'setHistoryList'
+    ]),
+    timeupdate (e) {
+      // console.log(e.target.currentTime)
+      this.currentTime = e.target.currentTime
+    },
+    end () {
+      if (this.modeType === mode.loop) {
+        this.setCurrentIndex(this.currentIndex + 1)
+      } else if (this.modeType === mode.one) {
+        this.$refs.audio.play()
+      } else if (this.modeType === mode.random) {
+        const index = getRandomIntInclusive(0, this.songs.length - 1)
+        this.setCurrentIndex(index)
       }
     }
   }
